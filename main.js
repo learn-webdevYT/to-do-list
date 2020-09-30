60fa0358b76456c9432db8b780381675a1ffeab6
AnirudhChopra2810 <65032965+AnirudhChopra2810@users.noreply.github.com>
2020-09-30 23:30:21 +0530

    Some Features added

--- main.js
+++ main.js
@@ -2,17 +2,78 @@ let addToDoButton = document.getElementById('addToDo');
 let toDoContainer = document.getElementById('toDoContainer');
 let inputField = document.getElementById('inputField');
 
-addToDoButton.addEventListener('click', function(){
-    var paragraph = document.createElement('p');
-    paragraph.classList.add('paragraph-styling');
-    paragraph.innerText = inputField.value;
-    toDoContainer.appendChild(paragraph);
-    inputField.value = "";
-    paragraph.addEventListener('click', function(){
-        paragraph.style.textDecoration = "line-through";
-    })
-    paragraph.addEventListener('dblclick', function(){
-        toDoContainer.removeChild(paragraph);
-    })
-})
+if (window.localStorage.getItem("todos") == undefined) {
+    let todos = [];
+    window.localStorage.setItem("todos", JSON.stringify(todos));
+}
 
+let save = window.localStorage.getItem("todos");
+let todos = JSON.parse(save);
+
+addToDoButton.addEventListener('click', function () {
+
+    if (inputField.value != "") {
+        createItem(inputField.value);
+        todos.push(inputField.value);
+        window.localStorage.setItem("todos", JSON.stringify(todos));
+        inputField.value = "";
+    }
+    else if (!inputField.value || inputField.value.trim() == "") {
+        alert("Invalid Input");
+        return;
+    }
+
+});
+
+function createItem(name) {
+
+    let divElement = document.createElement('div');
+    divElement.classList.add('item');
+
+    // text
+    let input = document.createElement('input');
+    input.classList.add('paragraph-styling');
+    input.setAttribute('style', 'color: grey');
+    input.disabled = true;
+    input.value = name;
+
+    // Remove button
+    let button = document.createElement('button');
+    button.classList.add('remove');
+    button.type = 'button';
+    button.innerHTML = 'REMOVE';
+    button.addEventListener('click', function () {
+        toDoContainer.removeChild(divElement);
+        let index = todos.indexOf(name);
+        todos.splice(index, 1);
+        window.localStorage.setItem("todos", JSON.stringify(todos));
+    });
+
+    //Edit Button
+    let editbutton = document.createElement('button');
+    editbutton.classList.add('edit');
+    editbutton.type = 'button';
+    editbutton.innerHTML = 'EDIT';
+    editbutton.addEventListener('click', function () {
+
+        if (input.disabled == true) {
+            input.disabled = !input.disabled;
+        }
+        else {
+            input.disabled = !input.disabled;
+        }
+        let indexOf = todos.indexOf(name);
+        todos[indexOf] = input.value;
+        window.localStorage.setItem("todos", JSON.stringify(todos));
+    });
+
+    // add to div element
+    toDoContainer.appendChild(divElement);
+    divElement.appendChild(input);
+    divElement.appendChild(button);
+    divElement.appendChild(editbutton);
+}
+
+for (let i = 0; i < todos.length; i++) {
+    createItem(todos[i]);
+}
